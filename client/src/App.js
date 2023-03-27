f24b01b1ecfa0ee516f8005b4563bfaead9e7ca1
Wie Kiang H <wiekiang@gmail.com>
2023-03-27 15:48:37 +1000

    project initialization

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    return (
+        <div className="App">
+            <h1>React App</h1>
+        </div>
+    );
+}
+
+export default App;
